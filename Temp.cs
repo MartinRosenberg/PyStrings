1e2e85e6930312eba0f924206e26d9189a8460a1
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2015-08-20 20:00:24 -0500

    Corrected Value being static.

--- Temp.cs
+++ Temp.cs
@@ -1,19 +1,19 @@
-using System;
+﻿using System;
 using System.Text;
 
 namespace PyString
 {
-	public class PyString
+	public class PyStringMarty
 	{
-		#region Values
-		
+		#region Value
+
 		public string Value { get; }
 
-		#endregion Values
+		#endregion Value
 
 		#region Constructors
 
-		public PyString(string value)
+		public PyStringMarty(string value)
 		{
 			Value = value;
 		}
@@ -22,11 +22,12 @@ namespace PyString
 
 		#region Implicit conversions
 
-		public static implicit operator string(PyString value)
-			=> value.ToString();
+		public static implicit operator string (PyStringMarty s) => s.ToString();
 
-		public static implicit operator PyString(string value)
-			=> new PyString(value);
+		public static implicit operator PyStringMarty(string s) => new PyStringMarty(s);
+
+		//public static explicit operator PyStringMarty(char[] s) => new PyStringMarty(new string(s));
+
 
 		#endregion Implicit conversions
 
@@ -34,24 +35,25 @@ namespace PyString
 
 		public override string ToString() => Value;
 
+		public override int GetHashCode() => Value.GetHashCode();
+
 		#endregion Overrides
 
-		#region Helper methods
+		public int Length => Value.Length;
 
+		// Convert Python indices to C# ones
 		private static int PythonizeIndex(int index)
-			=> index < 0 
+			=> index < 0
 				? Value.Length + index
 				: index;
-		
-		#endregion Helper methods
-		
-		#region Indexers
-		
-		public string this[int index] 
+
+		// Python substring, part 1
+		public string this[int index]
 			=> index == -1
 				? this[index, null]
 				: this[index, index + 1];
 
+		// Python substring, part 2
 		public string this[int? nMin = null, int? nMax = null, int? nStep = null]
 		{
 			get
@@ -64,10 +66,12 @@ namespace PyString
 				// Rebuild string - exception cases
 				if (step == 0)
 				{
+					// Special case 1 - a 0-step size isn't allowed, so throw an exception
 					throw new ArgumentOutOfRangeException(nameof(step), "Step size cannot be zero.");
 				}
 				if (Math.Sign(step) != Math.Sign(max - min))
 				{
+					// Special case 2 - step and bounds point in opposite directions
 					throw new ArgumentOutOfRangeException(nameof(step), "Step size is incorrectly signed for the given bounds values.");
 				}
 
@@ -84,7 +88,5 @@ namespace PyString
 				return result.ToString();
 			}
 		}
-
-		#endregion Indexers
 	}
 }
